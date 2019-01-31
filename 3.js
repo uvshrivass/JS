e981d9d0a463a9bc752d893129148987dc959daa
uvshrivass <uvshrivas@gmail.com>
2019-01-31 11:16:31 +0530

    Battery API

--- 3.js
+++
@@ -1,9 +0,0 @@
-a = 8
-b = 3
-x = a^b
-
-b=(a&b)<<1
-a=x
-x= a^b
-
-console.log('Sum is', x)
--- a.js
+++
@@ -1,8 +0,0 @@
-var a= 15;
-
-    if (a % 2== 0){
-        console.log ("Number is even.")
-    }
-    else {
-        console.log ("Number is odd.")
-    }

76fe439633bb0cc4b13f7c42ce51a99621d5cb95
uvshrivass <uvshrivas@gmail.com>
2019-03-30 12:51:40 +0530

    Advanced Javascript - Class

--- advanced-js/js-class.js
+++ advanced-js/js-class.js
@@ -1,6 +1,6 @@
 // Following demonstrates how a function can be used like a class in JavaScript
 // We can create an object of any function using "new" keyword
-// "Person" acts as a class and "person1" is its object
+// "Person" is a constructor function or acts as a class and "person1" is its object
 // Variable "this.firstname" & "this.lasttname" will act like properties
 
 function Person (){
